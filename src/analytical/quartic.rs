eb886ee73ae95a32742715827a9385ad2dce969c
vorot <mikhail.vorotilov@gmail.com>
2019-12-21 01:44:50 +0100

    Prepare simplifying discriminant

--- src/analytical/quartic.rs
+++ src/analytical/quartic.rs
@@ -25,6 +25,40 @@
 use super::super::FloatType;
 use super::super::Roots;
 
+/// Solves a quartic equation a4*x^4 + a4*x^3 + a2*x^2 + a1*x + a0 = 0.
+/// pp, rr, and dd are already computed while searching for multiple roots
+fn find_roots_via_depressed_quartic<F: FloatType>(a4: F, a3: F, a2: F, a1: F, a0: F, pp: F, rr: F, dd: F) -> Roots<F> {
+    // Depressed quartic
+    // https://en.wikipedia.org/wiki/Quartic_function#Converting_to_a_depressed_quartic
+
+    let _2 = F::from(2i16);
+    let _3 = F::from(3i16);
+    let _4 = F::from(4i16);
+    let _6 = F::from(6i16);
+    let _8 = F::from(8i16);
+    let _12 = F::from(12i16);
+    let _16 = F::from(16i16);
+    let _256 = F::from(256i16);
+
+    // a4*x^4 + a3*x^3 + a2*x^2 + a1*x + a0 = 0 => y^4 + p*y^2 + q*y + r.
+    let a4_pow_2 = a4 * a4;
+    let a4_pow_3 = a4_pow_2 * a4;
+    let a4_pow_4 = a4_pow_2 * a4_pow_2;
+    // Re-use pre-calculated values
+    let p = pp / (_8 * dbg!(a4_pow_2));
+    let q = rr / (_8 * dbg!(a4_pow_3));
+    let r = (dd + _16 * a4_pow_2 * (_12 * a0 * a4 - _3 * a1 * a3 + a2 * a2)) / (_256 * dbg!(a4_pow_4));
+
+    let mut roots = Roots::No([]);
+    for y in super::quartic_depressed::find_roots_quartic_depressed(dbg!(p), dbg!(q), dbg!(r))
+        .as_ref()
+        .iter()
+    {
+        roots = roots.add_new_root(dbg!(*y) - a3 / (_4 * a4));
+    }
+    roots
+}
+
 /// Solves a quartic equation a4*x^4 + a3*x^3 + a2*x^2 + a1*x + a0 = 0.
 ///
 /// Returned roots are ordered.
@@ -72,22 +106,22 @@ pub fn find_roots_quartic<F: FloatType>(a4: F, a3: F, a2: F, a1: F, a0: F) -> Ro
         let _256 = _192 + _64;
         // Discriminant
         // https://en.wikipedia.org/wiki/Quartic_function#Nature_of_the_roots
-        let discriminant = _256 * dbg!(a4) * a4 * a4 * a0 * a0 * a0
-            - _192 * a4 * a4 * dbg!(a3) * a1 * a0 * a0
-            - _128 * a4 * a4 * dbg!(a2) * a2 * a0 * a0
-            + _144 * a4 * a4 * a2 * dbg!(a1) * a1 * a0
-            - _27 * a4 * a4 * a1 * a1 * a1 * a1
-            + _144 * a4 * a3 * a3 * a2 * dbg!(a0) * a0
-            - _6 * a4 * a3 * a3 * a1 * a1 * a0
-            - _80 * a4 * a3 * a2 * a2 * a1 * a0
-            + _18 * a4 * a3 * a2 * a1 * a1 * a1
-            + _16 * a4 * a2 * a2 * a2 * a2 * a0
-            - _4 * a4 * a2 * a2 * a2 * a1 * a1
-            - _27 * a3 * a3 * a3 * a3 * a0 * a0
-            + _18 * a3 * a3 * a3 * a2 * a1 * a0
-            - _4 * a3 * a3 * a3 * a1 * a1 * a1
-            - _4 * a3 * a3 * a2 * a2 * a2 * a0
-            + a3 * a3 * a2 * a2 * a1 * a1;
+        let discriminant = dbg!(_256 * dbg!(a4) * a4 * a4 * a0 * a0 * a0)
+            - dbg!(_192 * a4 * a4 * dbg!(a3) * a1 * a0 * a0)
+            - dbg!(_128 * a4 * a4 * dbg!(a2) * a2 * a0 * a0)
+            + dbg!(_144 * a4 * a4 * a2 * dbg!(a1) * a1 * a0)
+            - dbg!(_27 * a4 * a4 * a1 * a1 * a1 * a1)
+            + dbg!(_144 * a4 * a3 * a3 * a2 * dbg!(a0) * a0)
+            - dbg!(_6 * a4 * a3 * a3 * a1 * a1 * a0)
+            - dbg!(_80 * a4 * a3 * a2 * a2 * a1 * a0)
+            + dbg!(_18 * a4 * a3 * a2 * a1 * a1 * a1)
+            + dbg!(_16 * a4 * a2 * a2 * a2 * a2 * a0)
+            - dbg!(_4 * a4 * a2 * a2 * a2 * a1 * a1)
+            - dbg!(_27 * a3 * a3 * a3 * a3 * a0 * a0)
+            + dbg!(_18 * a3 * a3 * a3 * a2 * a1 * a0)
+            - dbg!(_4 * a3 * a3 * a3 * a1 * a1 * a1)
+            - dbg!(_4 * a3 * a3 * a2 * a2 * a2 * a0)
+            + dbg!(a3 * a3 * a2 * a2 * a1 * a1);
         let pp = _8 * a4 * a2 - _3 * a3 * a3;
         let rr = a3 * a3 * a3 + _8 * a4 * a4 * a1 - _4 * a4 * a3 * a2;
         let delta0 = a2 * a2 - _3 * a3 * a1 + _12 * a4 * a0;
@@ -96,57 +130,47 @@ pub fn find_roots_quartic<F: FloatType>(a4: F, a3: F, a2: F, a1: F, a0: F) -> Ro
             - _3 * a3 * a3 * a3 * a3;
 
         // Handle special cases
-        let triple_root = dbg!(discriminant) == F::zero() && dbg!(delta0) == F::zero();
-        let quadruple_root = triple_root && dbg!(dd) == F::zero();
-        let no_roots = discriminant == F::zero() && dd == F::zero() && dbg!(pp) > F::zero() && dbg!(rr) == F::zero();
-        if dbg!(quadruple_root) {
-            // Wiki: all four roots are equal
-            Roots::One([-a3 / (_4 * a4)])
-        } else if dbg!(triple_root) {
-            // Wiki: At least three roots are equal to each other
-            // x0 is the unique root of the remainder of the Euclidean division of the quartic by its second derivative
-            //
-            // Solved by SymPy (ra is the desired reminder)
-            // a, b, c, d, e = symbols('a,b,c,d,e')
-            // f=a*x**4+b*x**3+c*x**2+d*x+e     // Quartic polynom
-            // g=6*a*x**2+3*b*x+c               // Second derivative
-            // q, r = div(f, g)                 // SymPy only finds the highest power
-            // simplify(f-(q*g+r)) == 0         // Verify the first division
-            // qa, ra = div(r/a,g/a)            // Workaround to get the second division
-            // simplify(f-((q+qa)*g+ra*a)) == 0 // Verify the second division
-            // solve(ra,x)
-            // ----- yields
-            // (−72*a^2*e+10*a*c^2−3*b^2*c)/(9*(8*a^2*d−4*a*b*c+b^3))
-            let x0 = (-_72 * a4 * a4 * a0 + _10 * a4 * a2 * a2 - _3 * a3 * a3 * a2)
-                / (_9 * (_8 * a4 * a4 * a1 - _4 * a4 * a3 * a2 + a3 * a3 * a3));
-            let roots = dbg!(Roots::One([x0]));
-            roots.add_new_root(dbg!(-(a3 / a4 + _3 * x0)))
-        } else if dbg!(no_roots) {
-            // Wiki: two complex conjugate double roots
-            Roots::No([])
-        } else {
-            // Depressed quartic
-            // https://en.wikipedia.org/wiki/Quartic_function#Converting_to_a_depressed_quartic
-
-            // a4*x^4 + a3*x^3 + a2*x^2 + a1*x + a0 = 0 => x^4 + a*x^3 + b*x^2 + c*x + d = 0.
-            let (a, b, c, d) = (a3 / a4, a2 / a4, a1 / a4, a0 / a4);
-            // x^4 + a*x^3 + b*x^2 + c*x + d = 0 => y^4 + p*y^2 + q*y + r.
-            let a_pow_2 = a * a;
-            let a_pow_3 = a_pow_2 * a;
-            let a_pow_4 = a_pow_2 * a_pow_2;
-            let subst = -a3 / (F::four() * a4);
-            let p = b - F::three() * a_pow_2 / _8;
-            let q = a_pow_3 / _8 - a * b / F::two() + c;
-            let r = d - F::three() * a_pow_4 / _256 - c * a / F::four() + a_pow_2 * b / _16;
-
-            let mut roots = Roots::No([]);
-            for x in super::quartic_depressed::find_roots_quartic_depressed(dbg!(p), dbg!(q), dbg!(r))
-                .as_ref()
-                .iter()
-            {
-                roots = roots.add_new_root(*x + subst);
+        let double_root = dbg!(discriminant) == F::zero();
+        if dbg!(double_root) {
+            let triple_root = double_root && dbg!(delta0) == F::zero();
+            let quadruple_root = triple_root && dbg!(dd) == F::zero();
+            let no_roots = dd == F::zero() && dbg!(pp) > F::zero() && dbg!(rr) == F::zero();
+            if dbg!(quadruple_root) {
+                // Wiki: all four roots are equal
+                Roots::One([-a3 / (_4 * a4)])
+            } else if dbg!(triple_root) {
+                // Wiki: At least three roots are equal to each other
+                // x0 is the unique root of the remainder of the Euclidean division of the quartic by its second derivative
+                //
+                // Solved by SymPy (ra is the desired reminder)
+                // a, b, c, d, e = symbols('a,b,c,d,e')
+                // f=a*x**4+b*x**3+c*x**2+d*x+e     // Quartic polynom
+                // g=6*a*x**2+3*b*x+c               // Second derivative
+                // q, r = div(f, g)                 // SymPy only finds the highest power
+                // simplify(f-(q*g+r)) == 0         // Verify the first division
+                // qa, ra = div(r/a,g/a)            // Workaround to get the second division
+                // simplify(f-((q+qa)*g+ra*a)) == 0 // Verify the second division
+                // solve(ra,x)
+                // ----- yields
+                // (−72*a^2*e+10*a*c^2−3*b^2*c)/(9*(8*a^2*d−4*a*b*c+b^3))
+                let x0 = (-_72 * a4 * a4 * a0 + _10 * a4 * a2 * a2 - _3 * a3 * a3 * a2)
+                    / (_9 * (_8 * a4 * a4 * a1 - _4 * a4 * a3 * a2 + a3 * a3 * a3));
+                let roots = dbg!(Roots::One([x0]));
+                roots.add_new_root(dbg!(-(a3 / a4 + _3 * x0)))
+            } else if dbg!(no_roots) {
+                // Wiki: two complex conjugate double roots
+                Roots::No([])
+            } else {
+                find_roots_via_depressed_quartic(a4, a3, a2, a1, a0, pp, dbg!(rr), dd)
             }
-            roots
+        } else {
+            let no_roots = dbg!(pp) > F::zero() || dbg!(dd) > F::zero();
+            if dbg!(no_roots) {
+                // Wiki: two pairs of non-real complex conjugate roots
+                Roots::No([])
+            } else {
+                find_roots_via_depressed_quartic(a4, a3, a2, a1, a0, pp, dbg!(rr), dd)
+            }
         }
     }
 }
@@ -187,7 +211,7 @@ mod test {
     fn test_find_roots_quartic_tim_luecke() {
         assert_eq!(
             find_roots_quartic(-14.0625f64, -3.75f64, 29.75f64, 4.0f64, -16.0f64),
-            Roots::Two([-1.1016117f64, 0.96827835f64])
+            Roots::Two([-1.1016116464173349f64, 0.9682783130840016f64])
         );
         assert_eq!(
             find_roots_quartic(-14.0625f32, -3.75f32, 29.75f32, 4.0f32, -16.0f32),
@@ -215,10 +239,10 @@ mod test {
             find_roots_quartic(2401f64, 2744f64, 1176f64, 224f64, 16f64),
             Roots::One([-0.2857142857142857f64])
         );
-        // 32-bit floating point is never accurate
+        // 32-bit floating point is less accurate
         assert_eq!(
             find_roots_quartic(2401f32, 2744f32, 1176f32, 224f32, 16f32),
-            Roots::Two([-0.29361892f32, -0.27799278f32])
+            Roots::One([-0.2857143f32])
         );
     }
 }
